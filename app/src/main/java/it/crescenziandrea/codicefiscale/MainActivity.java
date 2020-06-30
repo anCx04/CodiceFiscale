46213ddc60f0f91ce63bd0948ff8c340d47e7f48
Andrea <crescenziandrea@live.it>
2020-06-30 22:30:13 +0200

    Initial commit

---
+++ app/src/main/java/it/crescenziandrea/codicefiscale/MainActivity.java
@@ -0,0 +1,21 @@
+package it.crescenziandrea.codicefiscale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.google.android.material.imageview.ShapeableImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+       
+    }
+}
